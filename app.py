a0f74d900f1eceee85264bc1c27e04a6350d5b07
grantfanian <grantfanian@gmail.com>
2022-11-21 13:21:42 +0100

    reformat with black

--- app.py
+++ app.py
@@ -3,14 +3,24 @@ import flask
 import json
 import datetime
 from jinja2 import Environment, FileSystemLoader
+
 app = Flask("vkspy")
 
 
 def timefromutc(ls, tz=3):  # would be great to remove this
-    if type(ls)==int:
-        return str(datetime.datetime.utcfromtimestamp(ls)+datetime.timedelta(hours=tz))
-        
-    ls.update({"time":str(datetime.datetime.utcfromtimestamp(ls["time"])+datetime.timedelta(hours=tz))})
+    if type(ls) == int:
+        return str(
+            datetime.datetime.utcfromtimestamp(ls) + datetime.timedelta(hours=tz)
+        )
+
+    ls.update(
+        {
+            "time": str(
+                datetime.datetime.utcfromtimestamp(ls["time"])
+                + datetime.timedelta(hours=tz)
+            )
+        }
+    )
     return ls
 
 
@@ -22,12 +32,18 @@ def hello_world():
     with open("r.json", "rb") as file:
         j = file.read().decode("utf-8")
     d = json.loads(j)
-    cont = render_template("index.html", events=d, signs={
-                           "add": "+", "remove": "-", "change": "/"})
+    cont = render_template(
+        "index.html", events=d, signs={"add": "+", "remove": "-", "change": "/"}
+    )
     return cont
 
 
 @app.route("/robots.txt")
 def robots():
-    return flask.Response("""User-agent: *
-Disallow: /""".encode("ascii"), mimetype='text/plain')
+    return flask.Response(
+        """User-agent: *
+Disallow: /""".encode(
+            "ascii"
+        ),
+        mimetype="text/plain",
+    )
--- util.py
+++ util.py
@@ -3,6 +3,7 @@ import datetime
 import json
 import requests
 from dictdiffer import diff, patch
+
 # removed crop_photo and photo urls
 fields = "photo_id,verified,sex,bdate,city,country,home_town,has_photo,online,domain,has_mobile,contacts,site,education,universities,schools,status,last_seen,followers_count,common_count,occupation,nickname,relatives,relation,personal,connections,exports,activities,interests,music,movies,tv,books,games,about,quotes,can_post,can_see_all_posts,can_see_audio,can_write_private_message,can_send_friend_request,is_favorite,is_hidden_from_feed,timezone,screen_name,maiden_name,is_friend,friend_status,career,military,blacklisted,blacklisted_by_me,can_be_invited_group"
 
@@ -12,20 +13,19 @@ class VkError(Exception):
 
 
 class VkSession:
-    def __init__(self, version: str, token: str, base="https://api.vk.com/method/"):
+    def __init__(self, version: str, token: str, base:str="https://api.vk.com/method/"):
         self.prop = {"v": version, "access_token": token}
         self.base = base
 
     def exec(self, method: str, **kwargs):
         kwargs.update(self.prop)
-        resp = requests.get(self.base+method, params=kwargs)
+        resp = requests.get(self.base + method, params=kwargs)
         if resp.ok:
             result = resp.json()
-            if 'response' in result.keys():
+            if "response" in result.keys():
                 return result["response"]
             else:
-                raise VkError(
-                    f"  :(  \n{json.dumps(result,indent=1)}")
+                raise VkError(f"  :(  \n{json.dumps(result,indent=1)}")
         else:
             raise ValueError(f"server response: {resp.status_code}")
 
@@ -49,8 +49,7 @@ class Logger:
             data = {}
         data.update(update)
         with open(self.filename, "wb") as file:
-            file.write(json.dumps(data, ensure_ascii=False,
-                       indent=2).encode("utf-8"))
+            file.write(json.dumps(data, ensure_ascii=False, indent=2).encode("utf-8"))
 
     def append(self, last: dict, current: dict):
         if self.loaded:
@@ -62,12 +61,13 @@ class Logger:
             self.loaded = None
         if last != {} and last["online"] == current["online"]:
             # ignore last_seen until online changed
-            d = list(diff(last, current, ignore=set(['last_seen'])))
+            d = list(diff(last, current, ignore=set(["last_seen"])))
         else:
             d = list(diff(last, current))
         if len(d) > 0:
-            now = str(datetime.datetime.utcnow() +
-                      datetime.timedelta(hours=3))  # force GMT+3
+            now = str(
+                datetime.datetime.utcnow() + datetime.timedelta(hours=3)
+            )  # force GMT+3
             print(d)
             self.log[now] = d
             self.rewrite(self.log)
@@ -80,20 +80,24 @@ class SpyEye:
         self.session = session
         self.fields = fields
 
-    def gaze(self, user_id, logger: Logger, fields=fields):  # можно и домен и айди вроде
+    def gaze(
+        self, user_id, logger: Logger, fields=fields
+    ):  # можно и домен и айди вроде
         self.logger = logger
         self.user_id = user_id
         self.fields = fields
         while True:
             self.last = self.current.copy()
             self.current = self.session.exec(
-                "users.get", user_id=self.user_id, fields=self.fields)[0]
+                "users.get", user_id=self.user_id, fields=self.fields
+            )[0]
             logger.append(self.last.copy(), self.current.copy())
             time.sleep(20)
 
 
 if __name__ == "__main__":
     import os
+
     access_token, uid = os.environ["SIMPLESPY_TOKEN"], os.environ["SIMPLESPY_UID"]
     vk = VkSession("5.89", access_token)
     logger = Logger()
